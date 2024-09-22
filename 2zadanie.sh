3ce215511657c88492cd01ef1137436324c6e131
kvltyapka <kvltyapka@gmail.com>
2024-09-22 18:02:47 +0300

    Удалены ненужные файлы

--- 2zadanie.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sort -k2 -n /etc/protocols | tail -n 5 | awk '{print $2, $1}'
--- ident.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Проверка наличия аргумента
-if [ -z "$1" ]; then
-    echo "Usage: $0 filename"
-    exit 1
-fi
-
-grep -oE '\b[_a-zA-Z][_a-zA-Z0-9]*\b' "$1" | sort -u
